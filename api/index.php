302493549d26e2482aa4a56ccd860a1cda65d062
Luis Felipe <luisfelipesdn12@gmail.com>
2021-08-18 12:43:59 -0300

    chore(vercel): Set up project

---
+++ api/index.php
@@ -0,0 +1,8 @@
+
+<?php
+/**
+ * Here is the serverless function entry
+ * for deployment with Vercel.
+ * Know more: https://php.vercel.app
+ */
+require __DIR__.'/../public/index.php';
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <?php include 'includes/seo.html' ?>
+  <?php include 'includes/imports.html' ?>
+</head>
+
+<body class="bg-dark" style="--bs-bg-opacity: .95;">
+  <?php include 'includes/navbar.html'; ?>
+  <?php include 'includes/header.html'; ?>
+  <?php include 'includes/highlights.html'; ?>
+  <?php include 'includes/footer.html'; ?>
+</body>
+
+</html>
